47241c333316900e1d66c340948f5f61da087c84
Indro De <indro.de@gmail.com>
2015-06-28 19:22:03 +0200

    Fix generator

--- lib/feature_setting.rb
+++ lib/feature_setting.rb
@@ -2,7 +2,6 @@ require 'active_support/all'
 require 'feature_setting/version'
 require 'feature_setting/orm/active_record/fs_setting'
 require 'feature_setting/orm/active_record/fs_feature'
-require 'feature_setting/railtie' if defined?(Rails)
 
 module FeatureSetting
   # Your code goes here...
--- lib/generators/feature_setting/install/install_generator.rb
+++ lib/generators/feature_setting/install/install_generator.rb
@@ -3,22 +3,22 @@ require 'rails/generators/migration'
 require 'rails/generators/active_record'
 
 
-module FeatureSettings
+module FeatureSetting
   module Generators
 
     class InstallGenerator < Rails::Generators::Base
       include Rails::Generators::Migration
       extend ActiveRecord::Generators::Migration
 
-      # def self.next_migration_number(path)
-      #   ActiveRecord::Generators::Base.next_migration_number(path)
-      # end
+      def self.next_migration_number(path)
+        ActiveRecord::Generators::Base.next_migration_number(path)
+      end
 
       desc 'Generates database tables for feature_settings'
       source_root File.expand_path('../templates', __FILE__)
 
       def create_migrations
-        migration_name = 'create_feature_setting_tables.rb'
+        migration_name = 'create_fs_tables.rb'
         migration_template "migrations/#{migration_name}", "db/migrate/#{migration_name}"
       end
     end
