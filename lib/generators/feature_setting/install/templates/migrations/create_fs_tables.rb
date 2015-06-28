83c58365694545fe84f32b9d5f450ada0d49ab56
Indro De <indro.de@gmail.com>
2015-06-28 17:49:59 +0200

    Rename migration

--- lib/generators/feature_setting/install/templates/migrations/create_fs_tables.rb
+++ lib/generators/feature_setting/install/templates/migrations/create_fs_tables.rb
@@ -1,22 +1,15 @@
-class CreateFeatureSettingTables.rb < ActiveRecord::Migration
+class CreateFsTables < ActiveRecord::Migration
   def self.up
     create_table :fs_features do |t|
       t.string :key
       t.boolean :enabled, default: false
-
       t.timestamps null: false
     end
 
     create_table :fs_settings do |t|
       t.string :key
       t.string :value
-
       t.timestamps null: false
     end
-  end
-
-  def self.down
-    drop_table :fs_jobs
-    drop_table :fs_settings
   end
 end
