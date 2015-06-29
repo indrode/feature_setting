6dd381872abf25234653229198c7d4371901f79b
Indro De <indro.de@gmail.com>
2015-06-29 04:26:11 +0200

    Adjust migrations

--- lib/generators/feature_setting/install/templates/migrations/create_fs_tables.rb
+++ lib/generators/feature_setting/install/templates/migrations/create_fs_tables.rb
@@ -3,12 +3,15 @@ class CreateFsTables < ActiveRecord::Migration
     create_table :fs_features do |t|
       t.string :key
       t.boolean :enabled, default: false
+      t.string :klass
       t.timestamps null: false
     end
 
     create_table :fs_settings do |t|
       t.string :key
       t.string :value
+      t.string :value_type
+      t.string :klass
       t.timestamps null: false
     end
   end
