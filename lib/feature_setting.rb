2530a487e79fdf02915d7152dad6329610698089
Indro De <indro.de@gmail.com>
2019-03-20 16:14:17 +0100

    Remove byebug requirement

--- lib/feature_setting.rb
+++ lib/feature_setting.rb
@@ -2,7 +2,6 @@ require 'active_support/all'
 require 'feature_setting/version'
 require 'feature_setting/orm/active_record/fs_setting'
 require 'feature_setting/orm/active_record/fs_feature'
-require 'byebug'
 
 module FeatureSetting
 end
